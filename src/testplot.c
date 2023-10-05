44e665f46f8099e6b6341b50e22ebc0f923c21c6
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-10-05 17:28:51 +0200

    Plotting works now. Even dynamically.

--- src/testplot.c
+++ src/testplot.c
@@ -4,12 +4,10 @@
 
 #include "raylib.h"
 
-#define WIDTH 800
-#define HEIGHT 600
-
 size_t get_data(double* data_vec, size_t N) {
   static double ph = 0;
-  ph += 0.01;
+  // ph += 0.01;
+  ph += 0.0;
 
   for (size_t i=0; i<N; i++) {
     data_vec[i] = sin(2*(22/7)*0.05 * (double)i + ph);
@@ -41,14 +39,19 @@ double return_min(double *data_vec, size_t N) {
 }
 
 int main(void) {
+  int window_width = 800;
+  int window_height = 600;
   size_t N = 100;
   double *data_vec = (double*)malloc(sizeof(double) * N);
   
-  InitWindow(WIDTH, HEIGHT, "A test");
+  InitWindow(window_width, window_height, "A test");
+  SetWindowState(FLAG_WINDOW_RESIZABLE);
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     N = get_data(data_vec, N);
+    window_width = GetScreenWidth();
+    window_height = GetScreenHeight();
 
     double max = return_max(data_vec, N);
     double min = return_min(data_vec, N);
@@ -56,17 +59,41 @@ int main(void) {
 
     BeginDrawing();
       ClearBackground(WHITE);
+      // Vector2 startPos, endPos;
+      Vector2 startXaxis = {
+        .x = 0,
+        .y = (float)(-min/span*window_height)
+      };
+      Vector2 endXaxis = {
+        .x = (float)window_width,
+        .y = startXaxis.y
+      };
+      // Draw the x axis
+      DrawLineEx(startXaxis, endXaxis, 3.0, BLACK);
+      for (size_t i=0; i<N; i+=10) {
+        Vector2 tick_start = {
+          .x = (float)(i * (size_t)window_width/N),
+          .y = startXaxis.y
+        };
+        Vector2 tick_end = {
+          .x = tick_start.x,
+          .y = tick_start.y + (float)(window_height/100)
+        };
+        DrawLineEx(tick_start, tick_end, 3.0, BLACK);
+      }
+
+      // Draw the data
       for (size_t i=0; i<N; i++) {
-        int endPosX = (int)(i) * WIDTH/(int)N;
-        int endPosY = (int)((data_vec[i] - min)/span * HEIGHT);
+        int endPosX = (int)(i) * window_width/(int)N;
+        int endPosY = (int)((data_vec[i] - min)/span * window_height);
 
         if (i>0) {
-          int startPosX = (int)(i-1) * WIDTH/(int)N;
-          int startPosY = (int)((data_vec[i-1] - min)/span * HEIGHT);
+          int startPosX = (int)(i-1) * window_width/(int)N;
+          int startPosY = (int)((data_vec[i-1] - min)/span * window_height);
           DrawLine(startPosX, startPosY, endPosX, endPosY, BLUE);
         }
 
-        DrawCircle(endPosX, endPosY, 5, RED);
+        DrawCircle(endPosX, endPosY, 5, BLUE);
       }
     EndDrawing();
   }
